ac0ed694e6b2e22fa5584d72c81cb64f2a7ee302
Daniel <daniel_sousa.p@outlook.com>
2020-10-03 22:14:22 -0300

    feat: consuming api with getStaticProps and fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+export default function Home({ org }) {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>{org.login}</h1>
+      <h3>{org.description}</h3>
+
+      <p>Site: <a href={org.blog}>{org.blog}</a></p>
+    </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://api.github.com/orgs/rocketseat');
+  const data = await response.json();
+  
+  return {
+    props: {
+      org: data,
+    }
+  }
 }
