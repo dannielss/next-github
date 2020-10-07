024379b8ca607ddac77ed4fe6df21ff886efca13
Daniel <daniel_sousa.p@outlook.com>
2020-10-06 21:59:45 -0300

    using revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
-export default function Home({ org }) {
+import { GetStaticProps } from "next";
+
+export default function Home({ user }) {
   return (
     <div>
-      <h1>{org.login}</h1>
-      <h3>{org.description}</h3>
+      <h1>{user.name}</h1>
+      <h3>{user.bio}</h3>
 
-      <p>Site: <a href={org.blog}>{org.blog}</a></p>
+      <p>Site: <a href={user.blog}>{user.blog}</a></p>
     </div>
   )
 }
 
-export const getStaticProps = async () => {
-  const response = await fetch('https://api.github.com/orgs/rocketseat');
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://api.github.com/users/Daniels887');
   const data = await response.json();
   
   return {
     props: {
-      org: data,
-    }
+      user: data,
+    },
+    revalidate: 10
   }
 }
